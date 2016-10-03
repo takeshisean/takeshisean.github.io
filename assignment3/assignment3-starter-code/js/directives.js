98eb039cd40caf44541b2949fce99a10115312bf
takeshisean <takeshisean@hotmail.com>
2016-10-03 23:12:28 +0800

    Add files via upload

---
+++ assignment3/assignment3-starter-code/js/directives.js
@@ -0,0 +1,36 @@
+(function () {
+    'use strict';
+
+    angular.module('Directives', ['Constants'])
+        .directive('foundItems', FoundItemsDirective)
+        .directive('itemsLoaderIndicator', ItemsLoaderIndicatorDirective);
+
+
+    FoundItemsDirective.$inject = ['FOUND_TEMPLATE_PATH'];
+    function FoundItemsDirective(templatePath) {
+        return {
+            templateUrl: templatePath,
+            scope: {
+                items: '<',
+                onRemove: '&'
+            },
+            controller: FoundItemsDirectiveController,
+            controllerAs: 'found',
+            bindToController: true
+        };
+    }
+
+    function FoundItemsDirectiveController() {
+    }
+
+    ItemsLoaderIndicatorDirective.$inject = ['ITEMS_LOADER_INDICATOR_TEMPLATE_PATH'];
+    function ItemsLoaderIndicatorDirective(templatePath) {
+        return {
+            templateUrl: templatePath,
+            scope: {
+                itemsLoaderIndicator: '<',
+                pendingTimeout: '<'
+            }
+        };
+    }
+})();
