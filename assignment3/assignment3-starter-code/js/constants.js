28fdfae45ba23aa20d275505052d29d0927ae82f
kcyap <takeshisean@gmail.com>
2020-04-27 23:29:53 +0800

    repo clean up

--- assignment3/assignment3-starter-code/js/constants.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-    
-    angular.module('Constants', [])
-        .constant('API_PATH', 'https://davids-restaurant.herokuapp.com/menu_items.json')
-        .constant('DEFAULT_PENDING_TIMEOUT_FOR_EMPTY_SEARCH_TERM', 100)
-        .constant('FOUND_TEMPLATE_PATH', 'found.template.html')
-        .constant('ITEMS_LOADER_INDICATOR_TEMPLATE_PATH', 'loader/itemsloaderindicator.template.html');
-})();
