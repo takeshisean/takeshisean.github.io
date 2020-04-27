0562206735600886d5c7eb1f5f66f5a207c8873a
kcyap <takeshisean@gmail.com>
2020-04-27 23:33:24 +0800

    Repo clean up

--- LearnTypeScript/LearnTypeScript/Customer.js
+++
@@ -1,7 +0,0 @@
-var Customer = (function () {
-    function Customer() {
-        this.CustomerName = "";
-    }
-    return Customer;
-}());
-//# sourceMappingURL=Customer.js.map
--- LearnTypeScript/LearnTypeScript/Customer.ts
+++
@@ -1,3 +0,0 @@
-﻿class Customer {
-    public CustomerName: string = "";
-}
